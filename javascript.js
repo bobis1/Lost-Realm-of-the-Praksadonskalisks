3be14e2334c5b139089a0795390aeb00e9decf15
bobis <josephwaizy@gmail.com>
2025-09-17 20:02:13 -0500

    added more rocks

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
+var randomRock;
+let spaceW;
+let spaceH;
+let randomRock = document.getElementsByName("randomRock");
+
+
 function playAudio(audioClip) {
     new Audio(audioClip).play();
+
+}
+
+function remove($this) {
+  var element = $this;
+  element.remove();
+}
+
+function Init(){
+  spaceW = document.getElementById("background").height;
+  spaceH = document.getElementById("background").width;
+  document.getElementById("randomRock").style.top = Math.floor(Math.random() * spaceH) + "px";
+  randomRock.style.left = Math.floor(Math.random() * spaceW) + "px";
 }
